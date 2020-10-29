e465a061fc3b72883fda0ba8364dbb6a6d861606
Thomas Kim <Thomasjk0831@gmail.com>
2020-10-29 13:24:42 -0400

    wip2

--- api/server.js
+++ api/server.js
@@ -6,7 +6,9 @@ const apiRouter = require('./api-router.js');
 const server = express();
 
 server.use(helmet());
-
+server.get('/', (req,res) => {
+    res.status(200).json({msg: "welcome"})
+})
 server.use('/api', apiRouter);
 
 module.exports = server;
